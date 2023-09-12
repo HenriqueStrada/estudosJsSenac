10044141be018fed334a343fc752b23a51cb5e96
Henrique Strada <henriquestradax@gmail.com>
2023-09-11 21:44:07 -0300

    dale2

--- estudos.js
+++ estudos.js
@@ -10,12 +10,21 @@ console.log(arrTest);
 //
 let arr = [1, 3, 4, 6, 7, 9, 10];
 
-for(let i = 0; i < 7; i++){
+for(let i = 0; i < arr.length; i++){
     if(arr[i] % 2 == 0){
         arr[i] = arr[i] + 1;
     }else if (arr[i] %2 != 0){
         console.log("impar");
+        var ind = i;
+        arr.splice(i, 1);
+        i--;
     }
 }
 console.log(arr);
 
+var teste1 = {
+    nome : "Henrique",
+    email : "henrique@gmail.com",
+    idade : "16"
+}
+console.log(teste1);
